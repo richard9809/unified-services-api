913e9737d3feb81f94cd890e4b09c6bceaacc99c
rmulu-dast <rmulu@DAST.CO>
2025-11-11 18:24:36 -0800

    Initial commit

---
+++ src/test/java/com/unified_services_api/UnifiedServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unified_services_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedServicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
